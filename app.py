610e1a8f950f9129603e35c96b703f10bf7b994f
Sourav Jangid <67247180+chsouravjangid@users.noreply.github.com>
2024-09-29 19:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import numpy as np
 from flask import Flask,request,jsonify,render_template
 import pickle
 
-Flask_app = Flask(__name__)
+app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
-@Flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("index.html")
 
-@Flask_app.route("/predict", methods = ["POST"])
+@app.route("/predict", methods = ["POST"])
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
@@ -19,4 +19,4 @@ def predict():
     return render_template("index.html", prediction_text = "The Predicted Crop is {}".format(prediction))
 
 if __name__ == "__main__":
-    Flask_app.run()
+    app.run()
